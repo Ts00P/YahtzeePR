d9185c98dcc2c46ddd7a50f8c541ba35e2ef1cd3
Huib te Riet <0285227@student.rocvantwente.nl>
2018-04-25 19:02:34 +0200

    made a button function

---
+++ yahtzee/JavaScript.js
@@ -0,0 +1,9 @@
+﻿function start() {
+    var input1 = $("#usr1").val();
+    var input2 = $("#usr2").val();
+
+    $("#uitvoerNaam1").text(input1);
+    $("#uitvoerNaam2").text(input2);
+
+    $("#btn_Start").attr('value', 'Gooi')
+}
