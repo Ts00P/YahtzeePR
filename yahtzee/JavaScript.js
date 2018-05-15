a3596607f5e3de57817666df8aaf7bbee4115f1f
Teun <teun-steggink@hotmail.nl>
2018-05-15 09:36:43 +0200

    added random dobbel

--- yahtzee/JavaScript.js
+++ yahtzee/JavaScript.js
@@ -1,4 +1,4 @@
-﻿function start() {
+﻿function startSpel() {
     var input1 = $("#usr1").val();
     var input2 = $("#usr2").val();
 
@@ -6,7 +6,18 @@
     $("#uitvoerNaam2").text(input2);
 
     
-    $("#btn_Start").attr('value', (input1 + ' moet gooien'))
+    $("#btn_Start").attr('value', (input1 + ' moet gooien'));
 
-    $("#btn_Start").attr('value', (input2 + ' moet gooien'))
+    $("#btn_Start").attr('value', (input2 + ' moet gooien'));
+    dobbel();
+}
+
+function dobbel() {
+    
+    for (int i = 0; i < 6; i++) {
+        var random = Math.floor((Math.random() * 5) + 1);
+        $("#img" + i).attr('src', '../Images/' + random + '.png').load(function () {
+            this.width;
+        });
+    }
 }
