4cf940f1dc74c3c6c1fb992da5e6ecb03cccc851
Teun <teun-steggink@hotmail.nl>
2018-05-24 11:45:43 +0200

    Working fok and tok scores

--- yahtzee/JavaScript.js
+++ yahtzee/JavaScript.js
@@ -8,16 +8,14 @@ var beurt = 1;
 $(function () {
     $("img").click(function () {
 
-        if (start != true) {
-            return;
-        }
-
-        if ($.inArray(this.id, geselecteerd) != -1) {
-            $(this).css('border', "none");
-            geselecteerd.splice($.inArray(this.id, geselecteerd), 1);
-        } else {
-            geselecteerd.push(this.id);
-            $(this).css('border', "solid 2px blue");
+        if (start) {
+            if ($.inArray(this.id, geselecteerd) != -1) {
+                $(this).css('border', "none");
+                geselecteerd.splice($.inArray(this.id, geselecteerd), 1);
+            } else {
+                geselecteerd.push(this.id);
+                $(this).css('border', "solid 2px blue");
+            }
         }
     });
 });
@@ -25,11 +23,22 @@ $(function () {
 $(function () {
     $("input").click(function () {
         if (start) {
-            if (zoekID(this.id) == true) {
-                $(this).attr('disabled', 'disabled');
-                $(this).css({ "color": "white" });
-                veranderBeurt();
-            }
+
+            var clickID = this.id;
+            $('#scoreTable tbody tr').each(function () {
+                $(this).find('td input').each(function () {
+                    // Heeft de speler op een input geklikt in de score tabel?
+
+                    if (this.id.toString() == clickID.toString()) {
+                        // Heeft de speler op zijn eigen score colom geklikt?
+                        if (this.id.toString().split("-").pop() == "sp" + beurt) {
+                            $(this).attr('disabled', 'disabled');
+                            $(this).css({ "color": "red" });
+                            veranderBeurt();
+                        }
+                    }
+                });
+            });
         }
     });
 });
@@ -99,7 +108,6 @@ function berekenSimpeleWaardes() {
     var fours = 0;
     var fives = 0;
     var sixes = 0;
-    var threeOfAKind = 0;
 
     for (var i = 0; i < gegooid.length; i++) {
         var gooiWaarde = gegooid[i];
@@ -125,49 +133,57 @@ function berekenSimpeleWaardes() {
                 break;
         }
     }
-    var ScoreThreeOfKind = ThreeOfAKind(ones, twos, threes, fours, fives, sixes);
-    var ScoreFourOfKind = FourOfAKind(ones, twos, threes, fours, fives, sixes);
 
-    zetSimpeleWaardes(ones, twos, threes, fours, fives, sixes, ScoreThreeOfKind );
+    zetSimpeleWaardes(ones, twos, threes, fours, fives, sixes);
 }
 
-function zetSimpeleWaardes(ones, twos, threes, fours, fives, sixes, ScoreThreeOfKind) {
-    $("#one-sp" + beurt).val(ones);
-    $("#two-sp" + beurt).val(twos);
-    $("#three-sp" + beurt).val(threes);
-    $("#four-sp" + beurt).val(fours);
-    $("#five-sp" + beurt).val(fives);
-    $("#six-sp" + beurt).val(sixes);
+function zetSimpeleWaardes(ones, twos, threes, fours, fives, sixes) {
+    $('#scoreTable tbody tr').each(function () {
+        $(this).find('td input').each(function () {
+
+            if (!$(this).prop('disabled')) {
+                switch (this.id.toString()) {
+                    case "one-sp" + beurt:
+                        $(this).val(ones);
+                        break;
+                    case "two-sp" + beurt:
+                        $(this).val(twos);
+                        break;
+                    case "three-sp" + beurt:
+                        $(this).val(threes);
+                        break;
+                    case "four-sp" + beurt:
+                        $(this).val(fours);
+                        break;
+                    case "five-sp" + beurt:
+                        $(this).val(fives);
+                        break;
+                    case "six-sp" + beurt:
+                        $(this).val(sixes);
+                        break;
+                    case "tok-sp" + beurt:
+                        $(this).val(ThreeOfAKind(ones, twos, threes, fours, fives, sixes));
+                        break;
+                    case "fok-sp" + beurt:
+                        $(this).val(FourOfAKind(ones, twos, threes, fours, fives, sixes));
+                        break;
+                }
+            }
+        });
+    });
 }
 
 function resetWaardes() {
-
-    $("#one-sp" + beurt).val("");
-    $("#two-sp" + beurt).val("");
-    $("#three-sp" + beurt).val("");
-    $("#four-sp" + beurt).val("");
-    $("#five-sp" + beurt).val("");
-    $("#six-sp" + beurt).val("");
-    $("threeOfKind-sp" + beurt).val("");
+    $('#scoreTable tbody tr').each(function () {
+        $(this).find('td input').each(function () {
+            if (!$(this).prop('disabled')) {
+                $(this).val("");
+            }
+        });
+    });
 }
 
-function zoekID(theid) {
-    switch (theid) {
-        case "one-sp" + beurt:
-            return true;
-            break;
-        case "two-sp" + beurt:
-            return true;
-            break;
-        case "three-sp" + beurt:
-            return true;
-            break
-        default:
-            return false;
-    }
-}
-
-function ThreeOfAKind() {
+function ThreeOfAKind(ones, twos, threes, fours, fives, sixes) {
     var aantalgegooide_dobbelstenen = [ones, twos, threes, fours, fives, sixes];
     var ThreeOfKindpunten = 0;
     var ThreeOfKind = 0;
@@ -189,7 +205,7 @@ function ThreeOfAKind() {
     return ScoreThreeOfKind;
 }
 
-function FourOfAKind() {
+function FourOfAKind(ones, twos, threes, fours, fives, sixes) {
 
     var aantalgegooide_dobbelstenen = [ones, twos, threes, fours, fives, sixes];
     var FourOfKindpunten = 0;
@@ -197,7 +213,7 @@ function FourOfAKind() {
     var ScoreFourOfKind = 0;
 
     for (var i = 0; i < aantalgegooide_dobbelstenen.length; i++) {
-        if (aantalgegooide_dobbelstenen[i] == 3) {
+        if (aantalgegooide_dobbelstenen[i] == 4) {
             FourOfKindpunten += aantalgegooide_dobbelstenen[i] * (i + 1);
             FourOfKind = 1;
         }
